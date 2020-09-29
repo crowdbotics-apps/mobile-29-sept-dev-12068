ae289c7c28ab57499fe5817095b0e9eca17fdb83
Crowdbotics <team@crowdbotics.com>
2020-09-29 08:24:30 +0000

    Update models

--- backend/home/migrations/0004_customtext_dfdfasd.py
+++ backend/home/migrations/0004_customtext_dfdfasd.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0003_auto_20200929_0614'),
+        ("home", "0003_auto_20200929_0614"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='customtext',
-            name='dfdfasd',
+            model_name="customtext",
+            name="dfdfasd",
             field=models.BigIntegerField(blank=True, null=True),
         ),
     ]
